240751c6614f53629fc7b1c3c4023c3f465b5039
thephenom1708 <kmendki98@gmail.com>
2020-03-29 23:25:37 +0530

    Lucky draw done

--- lucky_draw/models.py
+++ lucky_draw/models.py
@@ -1,3 +1,5 @@
+from datetime import date, timedelta
+
 from django.db import models
 from django.conf import settings
 from accounts.models import User as UserModel
@@ -12,6 +14,7 @@ class LuckyDraw(models.Model):
     winner = models.ForeignKey(User, null=True, blank=True, related_name='winning_lucky_draws', on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
     order_amount_limit = models.DecimalField(max_digits=20, decimal_places=2, default=100.0)
+    orders_from_date = models.DateField()
     created_on = models.DateTimeField(auto_now_add=True)
     finished_on = models.DateTimeField(blank=True, null=True)
 
@@ -27,9 +30,10 @@ pre_save.connect(pre_save_lucky_draw_receiver, sender=LuckyDraw)
 
 
 def post_save_lucky_draw_receiver(sender, instance, *args, **kwargs):
+    print(instance.orders_from_date - date.today() - timedelta(days=1))
     for user in UserModel.objects.filter(active=True):
-        user_carts_sum = user.carts.filter(checkout=True).aggregate(Sum('subtotal'))['subtotal__sum'] or 0
-        if user_carts_sum >= instance.order_amount_limit:
+        user_carts_sum = user.carts.filter(checkout=True, updated__gte=instance.orders_from_date).aggregate(Sum('subtotal'))['subtotal__sum'] or 0
+        if user_carts_sum and user_carts_sum >= instance.order_amount_limit:
             LuckyDrawProfile.objects.get_or_create(user=user, lucky_draw=instance, order_amount=user_carts_sum)
         else:
             LuckyDrawProfile.objects.filter(user=user, lucky_draw=instance).delete()
